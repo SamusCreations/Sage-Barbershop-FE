b18b39311f941dbcdca66cab29017a7ac0cbf35a
SamusCreations <127237967+SamusCreations@users.noreply.github.com>
2024-05-24 17:21:18 -0600

    applied new structure and some basic components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { loggedGuard } from './core/guards/logged.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.AUTH_ROUTES),
+    canActivate: [loggedGuard],
+  },
+];
