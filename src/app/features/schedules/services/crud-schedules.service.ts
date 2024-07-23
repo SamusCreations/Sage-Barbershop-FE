d45ad075f2e6b6a31072c75afefb335a36112d2e
GeraldEDU19 <gercha32@gmail.com>
2024-07-23 10:51:35 -0600

    Schedule service is added

---
+++ src/app/features/schedules/services/crud-schedules.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../../../shared/httpService/http.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudSchedulesService {
+  private endpoint = 'schedule'; 
+
+  constructor(private httpService: HttpService) { }
+
+  getAll(): Observable<any> {
+    return this.httpService.get<any>(this.endpoint);
+  }
+
+  findById(id: number): Observable<any> {
+    const params = new URLSearchParams();
+    params.append('id', id.toString());
+    return this.httpService.get<any>(`${this.endpoint}/getById`, params);
+  }
+
+  findByBranch(id: number): Observable<any> {
+    const params = new URLSearchParams();
+    params.append('branchId', id.toString());
+    return this.httpService.get<any>(`${this.endpoint}/getByBranch`, params);
+  }
+
+  create(scheduleData: FormData): Observable<any> {
+    return this.httpService.post<any>(this.endpoint, scheduleData);
+  }
+
+}
