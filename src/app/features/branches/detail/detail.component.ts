33de63cfdb7539de3f75837ed742c35b09cfa39c
SamusCreations <127237967+SamusCreations@users.noreply.github.com>
2024-07-23 14:35:41 -0600

    branches details

--- src/app/features/branches/detail/detail.component.ts
+++ src/app/features/branches/detail/detail.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CrudBranchesService } from '../services/crud-branches.service';
+import { Subject, takeUntil } from 'rxjs';
+import {
+  NotificacionService,
+  messageType,
+} from '../../../shared/notification/notification.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.css'
+  styleUrl: './detail.component.scss',
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit, OnDestroy {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  branch: any = null;
+  charging: boolean = true;
+  idObject: number = 0;
 
+  constructor(
+    private activeRouter: ActivatedRoute,
+    private crudService: CrudBranchesService,
+    private noti: NotificacionService
+  ) {}
+
+  ngOnInit(): void {
+    this.activeRouter.params.subscribe((params) => {
+      this.idObject = params['id'];
+      if (this.idObject != undefined) {
+        this.crudService
+          .findById(this.idObject)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe(
+            (data) => {
+              this.branch = data;
+              this.charging = false;
+            },
+            (error) => {
+              console.error('Error fetching branch details:', error);
+              this.noti.message(
+                'Error',
+                `An error occurred: ${error.message}`,
+                messageType.error
+              );
+              this.charging = false;
+            }
+          );
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
